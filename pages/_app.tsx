44fd304eb1ba540f12cb320ec3dba2bab6c44aca
obonobo <ethanbnb@gmail.com>
2021-05-07 22:08:04 -0400

    Added placeholder login with Google button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,11 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>TodoMVC</title>
         <link rel="icon" href={urlFor("/favicon.ico")} />
+        {/* <script src="https://apis.google.com/js/platform.js" async defer /> */}
+        <meta
+          name="google-signin-client_id"
+          content="336107596492-q2ci0rdsbq8j001up87vbmlv7ma02mca.apps.googleusercontent.com"
+        />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,36 @@
+import GoogleLogin, { GoogleLoginResponse } from "react-google-login";
 import Todo from "../src/components/Todo";
 
+const googleClientId =
+  "336107596492-q2ci0rdsbq8j001up87vbmlv7ma02mca.apps.googleusercontent.com";
+
 export default function TodoMVC(): JSX.Element {
-  return <Todo />;
+  const onSuccess = (res: GoogleLoginResponse) => {
+    console.log(`[Login Sucess] result: ${JSON.stringify(res)}`);
+    console.log(`Current User: ${res.profileObj}`);
+  };
+
+  const onFailure = (res: GoogleLoginResponse) => {
+    console.log(`[Login Failed] result: ${JSON.stringify(res)}`);
+  };
+
+  return (
+    <div>
+      <Todo />
+      <GoogleLogin
+        clientId={googleClientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy="single_host_origin"
+        isSignedIn
+        style={{
+          position: "fixed",
+          top: "100vh",
+          zIndex: 2000,
+          marginTop: "1000px !important",
+        }}
+      />
+    </div>
+  );
 }
